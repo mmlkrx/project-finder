ef6c6df12cbc02f00067674bf098cf5bc93a2fa0
Brandon Hopkins <github email address>
2014-08-05 14:56:44 -0400

    Creates index page

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,16 @@ class Project < ActiveRecord::Base
   has_many :user_projects
   has_many :users, through: :user_projects
   
+  def self.in_planning
+    where(status: 'planning')
+  end
+
+  def self.in_progress
+    where(status: 'in_progress')
+  end
+
+  def self.completed
+    where(status: 'completed')
+  end
+
 end
