8d4f3e0055d0bee4f069d672f735988b891a9d1c
Brandon Hopkins <github email address>
2014-08-06 11:17:06 -0400

    some initial work on collaboration approval

--- app/controllers/user_projects_controller.rb
+++ app/controllers/user_projects_controller.rb
@@ -1,6 +1,7 @@
 class UserProjectsController < ApplicationController
 
   def approve_collaboration
+    binding.pry
     @user_project = UserProject.find_by(user_id: current_user.id, project_id: params[:project_id])
   end
 
