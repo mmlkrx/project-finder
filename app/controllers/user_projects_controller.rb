9c14d19cc95c63c47e444082354bfd0e1e12dbad
Brandon Hopkins <github email address>
2014-08-06 18:09:54 -0400

    Issue with notifications fixed

--- app/controllers/user_projects_controller.rb
+++ app/controllers/user_projects_controller.rb
@@ -6,12 +6,19 @@ class UserProjectsController < ApplicationController
     @user_project.save
     @user = User.find(params[:user_id])
     @project = Project.find(params[:project_id])
-    @user.notifications.build(content: "You're approved to work on #{@project.title}").save
+    @user.notifications.build(content: "#{current_user.name} has approved you to work on #{@project.title}").save
+    flash[:notice] = "#{@user.name} has been added to your team"
     redirect_to @project
   end
 
   def deny_collaboration
-
+    @user = User.find(params[:user_id])
+    @project = Project.find(params[:project_id])
+    @user_project = UserProject.find_by(user_id: params[:user_id], project_id: params[:project_id])
+    @user_project.destroy
+    @user.notifications.build(content: "Your request to work on #{@project.title} has been denied").save
+    flash[:notice] = "#{@user.name}'s request to join your team has been denied."
+    redirect_to @project
   end
 
 end
