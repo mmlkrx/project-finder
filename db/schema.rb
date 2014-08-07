c0a5f3da96a560db889ca6172d39790a8ec3e84e
Brandon Hopkins <github email address>
2014-08-07 19:51:14 -0400

    Admin can change status of projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140807212824) do
+ActiveRecord::Schema.define(version: 20140807220658) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20140807212824) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "rated",      default: false
+    t.boolean  "invitation", default: false
   end
 
   create_table "user_skills", force: true do |t|
