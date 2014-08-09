b4e1bba607a977a8d115b8d940411e9c71304ddd
Matthias Loker <mmloker@gmail.com>
2014-08-08 21:54:48 -0400

    project show page shows endorse button

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -7,8 +7,13 @@ class Skill < ActiveRecord::Base
   has_many :project_skills
   has_many :projects, through: :project_skills
 
-  def rated?(user)
-    UserSkill.where("user_id = ? AND skill_id = ?", user.id, self.id).pluck(:rated).first
+  def rated?(user, project)
+    # UserSkill.where("user_id = ? AND skill_id = ?", user.id, self.id).pluck(:rated).first
+    UserSkillProject.where("project_id = ? AND user_skill_id = ?", project.id, self.find_user_skill_id_by_user_id_and_skill_id(user, project)).pluck(:endorsed).first
+  end
+
+  def find_user_skill_id_by_user_id_and_skill_id(user, project)
+    project.user_skills.where("user_id = ? AND skill_id = ?", user.id, self.id).pluck(:id).first
   end
 
 end
